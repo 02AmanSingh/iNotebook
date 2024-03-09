a3619ecca28143d208b6f74c22c92d4df011f7de
02AmanSingh <amansingh02001@gmail.com>
2024-03-10 02:43:01 +0530

    created navbar and routes.
    Backend server and end points completed and frontend setup done. Integrated react router.

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () =>{
+    return(
+        <div style={{margin: '4rem 0 0 0'}}>
+            <h1>This is home.</h1>
+        </div>
+    )
+}
+
+export default Home;
