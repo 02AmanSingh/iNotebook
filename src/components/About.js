63cc0bac237260bad4aa2c2223ba75347b148b4c
02AmanSingh <amansingh02001@gmail.com>
2024-03-12 23:55:20 +0530

    Use Location hook implemented.
    It is used for making navbar links active at that time when the same page is open.

--- src/components/About.js
+++ src/components/About.js
@@ -1,17 +1,11 @@
-import React, { useContext, useEffect } from 'react'
-import noteContext from '../context/notes/NoteContext'
+import React from 'react'
 
 const About = () => {
-  const a = useContext(noteContext);
   
-  useEffect(() => {
-    a.update();
-    // eslint-disable-next-line
-  }, [])
   
   return (
     <div style={{margin: '4rem 0 0 0'}}>
-      <h1>This is About {a.state.name} and he is in {a.state.class} </h1>
+      <h1>This is About page.</h1>
     </div>
   )
 }
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,22 +1,10 @@
-import { useState } from "react";
+// import { useState } from "react";
 import noteContext from "./NoteContext";
 
 const NoteState = (props) =>{
-    const s1 = {
-        "name": "Harry",
-        "class": "4A",
-    }
-    const [state, setState] = useState(s1);
-    const update = () => {
-        setTimeout(() => {
-            setState({
-                "name": "Aman",
-                "class": "10C",
-            });
-        }, 1000);
-    };
+    
     return(
-        <noteContext.Provider value={{state, update}}>
+        <noteContext.Provider value={{}}>
             {props.children}
         </noteContext.Provider>
     )
