cdf442002ea0b9b39cfa6685f400ba552ece2743
02AmanSingh <amansingh02001@gmail.com>
2024-07-20 12:53:04 +0530

    spelling mistake "title"

--- src/components/Noteitem.js
+++ src/components/Noteitem.js
@@ -9,7 +9,7 @@ const Noteitem = (props) => {
     <div className='col-md-3 my-3' >
       <div className="card">
         <div className="card-body">
-          <h5 className='card-ttle'>{note.title}</h5>
+          <h5 className='card-title'>{note.title}</h5>
           <p className='description'>{note.description}</p>
           <button className='mx-2' ><i className="fa-solid fa-trash" onClick={()=>{deleteNote(note._id); props.showAlert("Deleted Successfully", "success");}}></i></button>
           <button><i className="fa-solid fa-pen-to-square" onClick={()=>{updateNote(note)}}></i></button>
